41eb7f9db3b47ebc5d0e0a08f70625ef9714d7c2
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-23 16:10:56 +0530

    modified to remove extra stuff

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import TicTacToe from "./pages/tictactoe/tictactoe"
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1> YO</h1>
-      <div> bye bye </div>
+      <TicTacToe/>
     </>
   )
 }
