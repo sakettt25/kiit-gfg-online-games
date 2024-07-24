5956d4bfa5ffcdc0b1b9ff821dcd30aacd63bbe6
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-24 16:30:46 +0530

    added images instead of Letters, and made it look nice

--- frontend/src/pages/tictactoe/square.jsx
+++ frontend/src/pages/tictactoe/square.jsx
@@ -1,7 +1,11 @@
 
-function Square({value, onSquareClick}){
-   
-    return <div className="square" onClick={onSquareClick}>{value}</div>
+function Square({ value, onSquareClick }) {
+    if (value == null)
+        return <div className="square" onClick={onSquareClick}>{value}</div>
+    else if (value == 'X')
+        return <img className="square-img" onClick={onSquareClick} src="src/assets/cross.png"></img>
+    else
+        return <img className="square-img" onClick={onSquareClick} src="src/assets/letter-o.png"></img>
 }
 
 export default Square
--- frontend/src/pages/tictactoe/tictactoe.jsx
+++ frontend/src/pages/tictactoe/tictactoe.jsx
@@ -1,8 +1,10 @@
 import './tictactoe.css'
 import Board from "./board"
 function TicTacToe(){
-return <div>
+return <div className='tictactoe'>
     <Board/>
+    {/* <a href="https://www.flaticon.com/free-icons/reject" title="reject icons">Reject icons created by meaicon - Flaticon</a> */}
+    {/* <a href="https://www.flaticon.com/free-icons/letter-o" title="letter o icons">Letter o icons created by Md Tanvirul Haque - Flaticon</a> */}
 </div>
 }
 
