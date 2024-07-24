0e0b91f7b8d7ac3a76d2e4850243b0ac6c15f823
phANTom2303 <anikavy.goenka@gmail.com>
2024-07-24 16:49:51 +0530

    Made it so that a button press decides which game to play

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import TicTacToe from "./pages/tictactoe/tictactoe"
 import Header from "./components/header/header"
+import { useState } from "react"
 function App() {
-  return (
-    <>
-      <Header/>
-      <TicTacToe/>
-    </>
-  )
+
+  const [choice, setChoice] = useState(null);
+  if (choice == 1) {
+    return (
+      <>
+        <Header />
+        <TicTacToe />
+      </>
+    );
+  }
+  else {
+    return (
+      <>
+        <Header />
+        <button onClick={() => setChoice(1)}> Play tic tac toe</button>
+      </>
+    );
+  }
+
 }
 
 export default App
--- frontend/src/pages/tictactoe/tictactoe.jsx
+++ frontend/src/pages/tictactoe/tictactoe.jsx
@@ -1,7 +1,7 @@
 import './tictactoe.css'
 import Board from "./board"
 function TicTacToe(){
-return <div className='tictactoe'>
+return <div className='game'>
     <Board/>
     {/* <a href="https://www.flaticon.com/free-icons/reject" title="reject icons">Reject icons created by meaicon - Flaticon</a> */}
     {/* <a href="https://www.flaticon.com/free-icons/letter-o" title="letter o icons">Letter o icons created by Md Tanvirul Haque - Flaticon</a> */}
