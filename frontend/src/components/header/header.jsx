c9392f9b810263e1b348b7a0b030b515a4bda4be
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-04 17:15:35 +0530

    Added some styling to the header coponent

--- frontend/src/components/header/header.jsx
+++ frontend/src/components/header/header.jsx
@@ -3,7 +3,7 @@ function Header(){
 return (
     <div className={styles.header}>
         <img src="src/assets/online game logo.png" alt="" />
-        <h1 className="title">Online Games</h1>
+        <h1 className="title">Mini Games</h1>
         <ul>
             <li>About</li>
             <li>Contact</li>
