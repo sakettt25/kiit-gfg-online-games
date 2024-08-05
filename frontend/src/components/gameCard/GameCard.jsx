322200f0121cf87d6981c5b78091075f33d3e606
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-05 19:51:33 +0530

    refined the description of each game card

--- frontend/src/components/gameCard/GameCard.jsx
+++ frontend/src/components/gameCard/GameCard.jsx
@@ -1,16 +1,22 @@
-import styles from './GameCard.module.css'
+import './GameCard.css'
 
-function GameCard({ onCardClick,imgPath,gameName, playerCount, gameDuration}) {
+function GameCard({ onCardClick, imgPath, gameName, playerCount, gameDuration }) {
     return (
-        <div className={styles.gameCard} onClick={onCardClick}>
+        <div className="gameCard" onClick={onCardClick}>
             <img src={imgPath} />
 
             <div className="cardDescription">
-                <span>{gameName} |</span>
+
+                <span id='gameName'>{gameName} </span>
+
                 <img src="src/assets/players-icon.svg" alt="" />
-                <span> {playerCount} |</span>
-                <img src="src/assets/timer-icon.svg" alt="" />
+                <span > {playerCount} </span>
+
+
+
+                <img id= 'timerIcon'src="src/assets/timer-icon.svg" alt="" />
                 <span>{gameDuration}</span>
+
             </div>
         </div>
     );
