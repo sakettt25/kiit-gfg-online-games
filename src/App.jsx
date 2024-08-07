e5e43012feca21b814704dcde1cfa07e96ea7ef5
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-07 23:41:02 +0530

    image test 2

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,8 @@ function App() {
         <Header text={"Mini Games"} onImgClick={() => setChoice(0)} />
         <div className="cardList">
           <GameCard onCardClick={() => setChoice(1)} imgPath={"https://img.icons8.com/?size=100&id=VPhTpNuoUQoj&format=png&color=000000"} gameName={"Tic Tac Toe"} playerCount={2} gameDuration={"1 min."} />
-          <GameCard gameName={"Battleship"} imgPath={"src/assets/images/banners/battleship banner.jpg"} playerCount={2} gameDuration={"20 min."} />
-          <GameCard gameName={"Connect 4"} imgPath={"src/assets/images/banners/connect 4 banner.jpg"} playerCount={2} gameDuration={"15 min."} />
+          <GameCard gameName={"Battleship"} imgPath={"public/assets/images/banners/battleship banner.jpg"} playerCount={2} gameDuration={"20 min."} />
+          {/* <GameCard gameName={"Connect 4"} imgPath={"assets/images/banners/connect 4 banner.jpg"} playerCount={2} gameDuration={"15 min."} />
           <GameCard gameName={"Ludo"} imgPath={"src/assets/images/banners/ludo banner-cropped.jpg"} playerCount={4} gameDuration={"45 min."} />
           <GameCard gameName={"S&L"} imgPath={"src/assets/images/banners/snake-and-ladders-banner.jpg"} playerCount={6} gameDuration={"30 min."} />
           <GameCard gameName={"Game Name"} imgPath={"src/assets/images/banners/placeholderbanner2.jpg"} playerCount={4} gameDuration={"5 min."} />
@@ -34,7 +34,7 @@ function App() {
           <GameCard gameName={"Game Name"} imgPath={"src/assets/images/banners/placeHolderBanner3.jpg"} playerCount={4} gameDuration={"5 min."} />
 
           <GameCard gameName={"Game Name"} imgPath={"src/assets/images/banners/placeholder-banner.jpg"} playerCount={4} gameDuration={"5 min."} />
-          <GameCard gameName={"Game Name"} imgPath={"src/assets/images/banners/placeHolderBanner.png"} playerCount={4} gameDuration={"5 min."} />
+          <GameCard gameName={"Game Name"} imgPath={"src/assets/images/banners/placeHolderBanner.png"} playerCount={4} gameDuration={"5 min."} /> */}
 
         </div>
       </>
