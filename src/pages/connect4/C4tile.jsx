48cabed1e22a999b6e81c99c49e8d11649591ff9
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-08 01:26:47 +0530

    created basic skeleton of a connect 4 box

---
+++ src/pages/connect4/C4tile.jsx
@@ -0,0 +1,6 @@
+function C4tile(){
+    return(
+        <div className="tile"></div>
+    );
+}
+export default C4tile;
--- src/pages/connect4/connect4.jsx
+++ src/pages/connect4/connect4.jsx
@@ -1,7 +1,58 @@
-function Connect4(){
+import './connect4.css'
+import C4tile from './C4tile';
+function Connect4() {
     return (
-        <div>
-            Connect 4
+        <div className='c4board'>
+
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+            <div className='column'>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+                <C4tile></C4tile>
+            </div>
+
         </div>
     );
 }
